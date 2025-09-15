45c84daf3c704b4851d2ba36c0dd85b90afb0c59
Jake Soenneker <jake@soenneker.com>
2025-09-15 16:42:01 -0500

    Initial commit

---
+++ src/BoxShadow.cs
@@ -0,0 +1,24 @@
+﻿using Intellenum;
+using Soenneker.Quark.Enums.GlobalKeywords;
+
+namespace Soenneker.Quark.Enums.BoxShadows;
+
+/// <summary>
+/// An HTML CSS keyword set in .NET enumeration form
+/// </summary>
+[Intellenum<string>]
+public partial class BoxShadow
+{
+    /// <summary>
+    /// No shadow is displayed. This is the default value.
+    /// </summary>
+    public static readonly BoxShadow None = new("none");
+
+    /// <summary>
+    /// The shadow is drawn inside the element's border box, creating an inner shadow effect.
+    /// This keyword can be used in combination with other box-shadow values.
+    /// </summary>
+    public static readonly BoxShadow Inset = new("inset");
+
+    public static implicit operator BoxShadow(GlobalKeyword style) => new(style.Value); // Value is the underlying string
+}
---
+++ test/Soenneker.Quark.Enums.BoxShadows.Tests/BoxShadowTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.BoxShadows.Tests;
+
+public sealed class BoxShadowTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
