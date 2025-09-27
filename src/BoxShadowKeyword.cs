7b8cd6d275d6fb7ba685362e075e6922ee2a8e69
Jake Soenneker <jake@soenneker.com>
2025-09-27 09:35:53 -0500

    rename

--- src/BoxShadowKeyword.cs
+++ src/BoxShadowKeyword.cs
@@ -6,18 +6,18 @@ namespace Soenneker.Quark.Enums;
 /// An HTML CSS keyword set in .NET enumeration form
 /// </summary>
 [Intellenum<string>]
-public partial class BoxShadow
+public partial class BoxShadowKeyword
 {
     /// <summary>
     /// No shadow is displayed. This is the default value.
     /// </summary>
-    public static readonly BoxShadow None = new("none");
+    public static readonly BoxShadowKeyword None = new("none");
 
     /// <summary>
     /// The shadow is drawn inside the element's border box, creating an inner shadow effect.
     /// This keyword can be used in combination with other box-shadow values.
     /// </summary>
-    public static readonly BoxShadow Inset = new("inset");
+    public static readonly BoxShadowKeyword Inset = new("inset");
 
-    public static implicit operator BoxShadow(GlobalKeyword style) => new(style.Value); // Value is the underlying string
+    public static implicit operator BoxShadowKeyword(GlobalKeyword style) => new(style.Value); // Value is the underlying string
 }
